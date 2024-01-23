50b36e0e4faf2a88fee64b5639d639fcf1ba91be
Nauman Ahmed <35535536+nauman-ahmed@users.noreply.github.com>
2024-01-23 22:09:28 +0100

    removed main section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,6 @@ export default function Home() {
     return (
         <div>
             <Navbar   />
-            <MainSection  />
             <WorkFlowSection />
             <ChooseUsSection />
             <BrandsSection />
